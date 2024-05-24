3411b84bfb8da844559d936d1fd8fc3aa619de92
speedyes <speedyes2538@proton.me>
2024-05-24 20:29:37 +0200

    further changes for testing purposes

--- scripts/pppwnwrt11MIPS.sh
+++ scripts/pppwnwrt11MIPS.sh
@@ -2,7 +2,7 @@
 # Written by FalsePhilosopher
 opkg update
 opkg install unzip coreutils-rm libpcap1
-cd /
-wget https://github.com/FalsePhilosopher/PPPwnWRT/raw/main/PPPwnWRT11MIPS.zip
-unzip PPPwnWRT11MIPS.zip
+cd /tmp
+wget https://github.com/speedyes/PPPwnWRT/raw/main/PPPwnWRT11MIPS.zip
+unzip -d / PPPwnWRT11MIPS.zip
 rm PPPwnWRT11MIPS.zip
