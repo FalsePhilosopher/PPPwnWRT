dd2b466c88de47f79f2d0647fde42c654e497256
speedyes <speedyes2538@proton.me>
2024-05-25 00:53:40 +0200

    removed useless folder and enabled autostart

--- scripts/pppwnwrt11MIPS.sh
+++ scripts/pppwnwrt11MIPS.sh
@@ -6,3 +6,5 @@ cd /tmp
 wget https://github.com/speedyes/PPPwnWRT/raw/main/zips/PPPwnWRT11MIPS.zip
 unzip -d / PPPwnWRT11MIPS.zip
 rm PPPwnWRT11MIPS.zip
+/etc/init.d/pppwnwrt enable
+/etc/init.d/pppwnwrt start
