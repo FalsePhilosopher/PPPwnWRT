d519752027bab40df627a36d2fdfa8ca5a2e5700
speedyes <speedyes2538@proton.me>
2024-05-25 12:59:39 +0200

    changed file structure, zips and scripts
    now common files are in one zip and architecture-specific files are in their own zip files, thus reducing zip file count and complexity. Also removed script auto-start because variables need to be changed by the end user. They need to enable auto-start (that's already in init.d) manually after changing interface, stage1.bin and firmware version

--- scripts/pppwnwrt11MIPS.sh
+++ scripts/pppwnwrt11MIPS.sh
@@ -3,8 +3,10 @@
 opkg update
 opkg install unzip coreutils-rm libpcap1
 cd /tmp
-wget https://github.com/speedyes/PPPwnWRT/raw/main/zips/PPPwnWRT11MIPS.zip
-unzip -d / PPPwnWRT11MIPS.zip
-rm PPPwnWRT11MIPS.zip
-/etc/init.d/pppwnwrt enable
-/etc/init.d/pppwnwrt start
+wget https://github.com/speedyes/PPPwnWRT/raw/main/zips/files.zip
+wget https://github.com/speedyes/PPPwnWRT/raw/main/zips/MIPS.zip
+unzip -d / files.zip MIPS.zip
+rm files.zip MIPS.zip
+pppwn list
+echo ""
+echo "Change stage1.bin, firmware version and interface accordingly in '/etc/pppwnwrt/pppwnwrt.sh'! Start the script AFTER you change these variables!"
