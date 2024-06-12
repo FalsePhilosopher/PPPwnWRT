8fccdce07d0d72c57bb99261cb71b24f09c80d16
speedyes <104528844+speedyes@users.noreply.github.com>
2024-06-12 14:33:32 +0200

    bump PPPwn C++ to v1.0.0
    this results in smaller binary files. my guess is that nightly build includes debug features. also changed filename from x64.zip to x86_64.zip

--- scripts/x64.sh
+++ scripts/x64.sh
@@ -4,10 +4,10 @@ opkg update
 opkg install unzip coreutils-rm libpcap1
 cd /tmp
 wget https://github.com/FalsePhilosopher/PPPwnWRT/raw/main/zips/files.zip
-wget https://github.com/FalsePhilosopher/PPPwnWRT/raw/main/zips/x64.zip
+wget https://github.com/FalsePhilosopher/PPPwnWRT/raw/main/zips/x86_64.zip
 unzip -d / files.zip
-unzip -d / x64.zip
-rm files.zip x64.zip
+unzip -d / x86_64.zip
+rm files.zip x86_64.zip
 pppwn list
 echo ""
 echo -e "\e[1;31mChange stage1.bin, firmware version and interface accordingly in '/etc/pppwnwrt/pppwnwrt.sh'! Start the script AFTER you change these variables! \e[0m"
